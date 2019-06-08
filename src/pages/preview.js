3ae703ae09e26d2871c024fc07037be7ebc75e27
Benjamin Knight <ben@benknight.me>
2019-06-08 15:49:30 +0700

    Remove unused page

--- src/pages/preview.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Page from '../components/Page';
-
-export default props => (
-  <Page className="pa4 mw7 center" isPrivate {...props}>
-    <h1>Coming soon</h1>
-  </Page>
-);
