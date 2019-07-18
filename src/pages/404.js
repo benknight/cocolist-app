893e8dfc5ee22ab1b03efbe33e6fd6cdd3a0b63c
Benjamin Knight <ben@benknight.me>
2019-07-18 14:44:26 +0700

    Add Header to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import Header from '../components/Header';
 
 export default props => (
-  <div className="ph4 pv6 tc">
-    <h1>This page has moved or is no longer available.</h1>
-  </div>
+  <>
+    <Helmet>
+      <title>Not found</title>
+    </Helmet>
+    <Header location={props.location} />
+    <div className="ph4 pv6 tc">
+      <h1>This page has moved or is no longer available.</h1>
+    </div>
+  </>
 );
