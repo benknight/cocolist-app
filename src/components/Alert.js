c377f19661b318368bcf8756f0c7ed5cc577625c
Benjamin Knight <ben@benknight.me>
2019-09-09 15:25:37 +0700

    Add Alert component with dismissal behavior

---
+++ src/components/Alert.js
@@ -0,0 +1,38 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { injectIntl } from 'react-intl';
+import { TextButton } from '@cocolist/thumbprint-react';
+import { NavigationCloseSmall } from '@thumbtack/thumbprint-icons';
+
+function Alert({ intl: { formatMessage }, ...props }) {
+  const storageKey = `${props.id}_dismissed`;
+  const [isHidden, hide] = useState(window.localStorage.getItem(storageKey));
+  return (
+    <div
+      className={cx('relative z-3 tp-alert tp-alert--banner', props.className, {
+        dn: isHidden,
+      })}>
+      <div className="tp-alert__text">{props.children}</div>
+      <div className="absolute top0 right1 bottom0 pa2 tp-body-3 white flex items-center">
+        <TextButton
+          accessibilityLabel={formatMessage({ id: 'dismiss_action_label' })}
+          iconLeft={<NavigationCloseSmall />}
+          onClick={() => {
+            hide(true);
+            window.localStorage.setItem(storageKey, true);
+          }}
+          theme="inherit"
+        />
+      </div>
+    </div>
+  );
+}
+
+Alert.propTypes = {
+  id: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default injectIntl(Alert);
