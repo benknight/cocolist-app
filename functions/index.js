b0fd537e6e578ede6132757699f4300f8a66809c
Benjamin Knight <ben@benknight.me>
2019-12-22 11:58:59 +0700

    Add deploy function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 // https://firebase.google.com/docs/functions/write-firebase-functions
+const axios = require('axios');
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
@@ -90,5 +91,36 @@ exports.rejectReview = functions.https.onRequest(async (req, res) => {
       console.error(error);
     }
     res.status(500).send('Error');
+  }
+});
+
+const githubToken = functions.config().github.token;
+const deploySecret = functions.config().deploy.secret;
+
+exports.deploy = functions.https.onRequest(async (req, res) => {
+  if (req.query.secret !== deploySecret) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+  try {
+    await axios.post(
+      'https://api.github.com/repos/benknight/cocolist/dispatches',
+      {
+        event_type: 'deploy',
+      },
+      {
+        auth: {
+          username: 'benknight',
+          password: githubToken,
+        },
+        headers: { Accept: 'application/vnd.github.everest-preview+json' },
+      },
+    );
+    res.status(200).send('Success');
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+    return;
   }
 });
