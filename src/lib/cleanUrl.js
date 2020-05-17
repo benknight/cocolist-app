f7a3b590abee122767365f931711332825b8b24c
Benjamin Knight <ben@benknight.me>
2020-05-17 12:25:56 +0700

    Update cleanUrl.js

--- src/lib/cleanUrl.js
+++ src/lib/cleanUrl.js
@@ -1,6 +1,7 @@
 export default function cleanUrl(url) {
   return url
-    .replace(/\/$/, '')
-    .replace(/https?:\/\//, '')
-    .replace(/www\./, '');
+    .replace(/https?:\/\//, '') // remove protocol
+    .replace(/www\./, '') // remove www
+    .replace(/\?.+/, '') // remove query string
+    .replace(/\/$/, ''); // remove trailing slash
 }
